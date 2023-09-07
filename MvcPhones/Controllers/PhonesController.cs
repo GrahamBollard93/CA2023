e7e641ff44f674b6e2fb402269a9d46997d25bdb
GrahamBollard93 <10617126@mydbs.ie>
2023-09-07 23:18:00 +0100

    HttpClient - worked!

--- MvcPhones/Controllers/PhonesController.cs
+++ MvcPhones/Controllers/PhonesController.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcPhones.Data;
 using MvcPhones.Models;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+
 
 
 namespace MvcPhones.Controllers
@@ -20,71 +12,26 @@ namespace MvcPhones.Controllers
     {
         private readonly MvcPhonesDbContext _context;
         private readonly IHttpClientFactory _clientFactory;
-        private readonly UserManager<IdentityUser> _userManager;
+
         private IEnumerable<Phones>? allPhones;
 
-        public PhonesController(MvcPhonesDbContext context, IHttpClientFactory clientFactory, UserManager<IdentityUser> userManager)
+        public PhonesController(MvcPhonesDbContext context, IHttpClientFactory clientFactory)
         {
             _context = context;
             _clientFactory = clientFactory;
-            _userManager = userManager;
         }
 
 
-        public async Task<IActionResult> Index(string? searchString, string sortOrder, int page = 1)
+        public async Task<IActionResult> Index()
         {
-            int pageSize = 6; // Number of products per page
-
             HttpClient client = _clientFactory.CreateClient(name: "PhonesApi");
-
-            HttpRequestMessage request = new(
-                method: HttpMethod.Get,
-                requestUri: "api/v1/phones"
-            );
-
+            HttpRequestMessage request = new(method: HttpMethod.Get, requestUri:
+            "/api/v1/phones");
             HttpResponseMessage response = await client.SendAsync(request);
-
-            IEnumerable<Phones>? allProducts = await response.Content.ReadFromJsonAsync<IEnumerable<Phones>>();
-
-            ViewData["CurrentSort"] = sortOrder;
-            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : ""; // If the sortOrder variable is null or empty the retrun "name_desc", or else return nothing
-              
-
-            // Calculate the total number of pages
-            int totalItems = allProducts!.Count();
-            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
-
-            // Ensure the requested page is within the valid range
-            page = Math.Max(1, Math.Min(page, totalPages));
-
-            // Retrieve the subset of products for the requested page
-            IEnumerable<Phones>? phones = allPhones?
-                .OrderBy(p => p.Brand)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize);
-
-            var pagedResult = new PagedResult<Phones>
-            {
-                PageIndex = page,
-                PageSize = pageSize,
-                TotalItems = totalItems,
-                TotalPages = totalPages,
-                Items = phones
-            };
-
-             switch (sortOrder)
-            {
-                case "Brand":
-                    allPhones = allPhones?.OrderByDescending(m => m.Brand);
-                    break;
-                case "League":
-                    allPhones = allPhones?.OrderBy(m => m.Model);
-                    break;
-            }
-
-            return View(pagedResult);
+            IEnumerable<Phones>? model = await response.Content.ReadFromJsonAsync<IEnumerable<Phones>>();
+            return View(model);
         }
     }
 
-    
+
 }
